d39d084d3723f3cf2034b8c3e249dd1a6c5b3c7b
Walle <Wallinder@users.noreply.github.com>
2025-07-19 19:26:47 +0200

    remove duplicate struct key

--- internal/state/state.go
+++ internal/state/state.go
@@ -1,15 +1,10 @@
 package state
 
 type SessionState struct {
-	SessionId string
 	Seq       int64
 	Running   bool
 	Metadata  Metadata
 	ReadyData ReadyData
-}
-
-func (s *SessionState) UpdateSessionId(sessionId string) {
-	s.SessionId = sessionId
 }
 
 func (s *SessionState) UpdateSeq(seq int64) {
